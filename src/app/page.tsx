c82c4a15630ea55368b3a8340cd601b3bd8b52c4
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-08-28 17:17:17 -0300

    Update FeedPage param handling and redirect logic
    Removed 'page' from searchParams and updated redirect condition to check for 'username' and 'slug' instead of 'page'. This simplifies the parameter structure and improves redirect accuracy.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { Feed } from '../components/feed'
 export default async function FeedPage({
   searchParams,
 }: {
-  searchParams: Promise<{ page: string; username: string; slug: string }>
+  searchParams: Promise<{ username: string; slug: string }>
 }) {
-  const { page, username, slug } = await searchParams
+  const { username, slug } = await searchParams
 
   // const user = await fetchAuthedUser()
 
-  if (page === 'content') {
+  if (username && slug) {
     redirect(`/${username}/${slug}`)
   }
 
